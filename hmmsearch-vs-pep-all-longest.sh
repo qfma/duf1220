6fc7e40a1cfc4aa528d52cb7023032cd41769f26
Fabian Zimmer <fabian@qfma.de>
2015-01-14 13:10:11 +0000

    Added wrapper script to search all proteomes using hmmsearch

---
+++ hmmsearch-vs-pep-all-longest.sh
@@ -0,0 +1,10 @@
+NOW=$(date +"%Y-%m-%d")
+OUTFOLDER="$NOW-hmmsearch-vs-pep-all-longest"
+for i in ./sequences/pep/*.pep.all.longest.fa;
+	do
+		SPECIES=$(echo $(basename $i) | cut -f1 -d.);
+		hmmsearch --domtblout "duf1220-vs-$SPECIES.domtblout" --tblout "duf1220-vs-$SPECIES.tblout" -E 1e-10 --cpu=8 ./pfam_hmm/PF06758_seed.hmm $i;
+	done;
+mkdir $OUTFOLDER
+mv *.tblout $OUTFOLDER
+mv *.domtblout $OUTFOLDER
