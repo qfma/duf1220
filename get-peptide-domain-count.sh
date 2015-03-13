62f2ad21bcd24d1ea5f46a0450ade2d639c61592
Fabian Zimmer <fabian@qfma.de>
2015-03-13 14:18:23 +0000

    renamed variables, removed variable

--- get-peptide-domain-count.sh
+++ get-peptide-domain-count.sh
@@ -2,10 +2,10 @@ echo "====PEP-all-longest===="
 echo "======================="
 
 NOW=$(date +"%Y-%m-%d")
-NOW="2015-01-23"
-for dfam in $NOW-hmmsearch-vs-pep-all-longest/*.domtblout;
+for domtblout in $NOW-hmmsearch-vs-pep-all-longest/*.domtblout;
 	do 
-		SPECIES=$(echo $(basename $dfam) | cut -f1 -d. | cut -f3 -d-)
-		domains=$(cat $dfam | grep -v "#" | wc -l);
+		SPECIES=$(echo $(basename $domtblout) | cut -f1 -d. | cut -f3 -d-)
+		# Sum the tblout reg column, non overlapping regions.
+		domains=$(cat $domtblout | grep -v "#" | wc -l);
 		echo $SPECIES: $domains
 	done;
