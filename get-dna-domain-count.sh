76704a20a9450eade0fd803c9f315d5c530c1713
Fabian Zimmer <fabian@qfma.de>
2015-03-13 14:14:01 +0000

    Removed deprecated files that are not included in the final analysis

--- get-dna-domain-count.sh
+++
@@ -1,7 +0,0 @@
-NOW=$(date +"%Y-%m-%d")
-for dfam in $1/*.tblout;
-	do
-		SPECIES=$(echo $(basename $dfam) | cut -f1 -d. | cut -f5 -d-)
-		domains=$(cat $dfam | grep -v "#" | wc -l);
-		echo $SPECIES: $domains
-	done;
