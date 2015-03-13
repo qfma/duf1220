baa26df4988d720cb0a2773a0f72fbfe544d9961
Fabian Zimmer <fabian@qfma.de>
2015-03-13 14:29:45 +0000

    Added comments

--- find-cm-genes.sh
+++ find-cm-genes.sh
@@ -1,20 +1,24 @@
 NOW=$(date +"%Y-%m-%d")
-# NOW="2015-01-23"
 OUTDIR="$NOW-genes-1000bp-updown"
 
-for tblout in $NOW-nhmmer-vs-cdna-longest/*.tblout;
-	do 
+
+# Extract the gene names from the tblout files and return the sequence using
+# ENSEMBL
+for tblout in $NOW-nhmmer-vs-cdna-longest/*.tblout; do 
 		SPECIES=$(echo $(basename $tblout) | cut -f1 -d.);
 		cat $tblout | \
 		grep -v "#" | \
 		awk '{print $18}' | \
 		cut -f2 -d":" | \
 		sort | uniq | ./gseq-flank
-	done;
+done
 
+# Move the results into a folder
 mkdir $OUTDIR
 mv *.1000bp.updown.fa $OUTDIR
 
+# Now run nhmmer for each gene against the CM promoter model and determine
+# if a CM hit is present
 for i in $OUTDIR/*.1000bp.updown.fa; do
 	GENE=$(echo $(basename $i) | cut -f1 -d.);
 	nhmmer --dfamtblout "$GENE.dfamtblout" \
@@ -25,9 +29,11 @@ for i in $OUTDIR/*.1000bp.updown.fa; do
 		   $i;
 done
 
+# Move the results into a folder
 mv *.dfamtblout $OUTDIR
 mv *.tblout $OUTDIR
 
+# Print the CM counts for each gene
 for i in $OUTDIR/*.tblout; do
 	GENE=$(echo $(basename $i) | cut -f1 -d.);
 	GENECOUNT=$(cat $i| grep -v "#" | wc -l)
